819ac709bf584539cc59ba2cbfb9a41eeb826677
Nate Annau <nateannau@gmail.com>
2025-01-21 00:40:02 -0800

    Start server

--- backend/main.py
+++
@@ -1,5 +0,0 @@
-import whisper
-
-model = whisper.load_model("turbo")
-result = model.transcribe("../data/accent_guy.mp3")
-print(result["text"])
--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,11 @@
 from flask import Flask, request
+import whisper  # type: ignore
+from threading import Thread
 
+
+model = whisper.load_model('base')
 app = Flask(__name__)
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -14,6 +19,15 @@ def upload_file():
     # Save the file to a directory
     file.save(f"./uploads/{file.filename}")
     return "File uploaded successfully", 200
+
+
+@app.route('/transcribe', methods=['POST'])
+def post_transcript():
+    # create transcript
+    transcript = model.transcribe(f"./uploads/{file.filename}")
+
+    print(transcript['text'])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
