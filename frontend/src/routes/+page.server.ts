83c658c862c7980deb9f4dc368c5af8698543f4e
Nate Annau <nateannau@gmail.com>
2025-01-21 00:46:02 -0800

    Localhost -> home

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -24,7 +24,7 @@ export const actions = {
 
         } else {
             const uploadFormData = await request.formData();
-            const response = await fetch("http://localhost:5000/upload", {
+            const response = await fetch("http://127.0.0.1:5000/upload", {
                 method: "POST",
                 body: uploadFormData,
             });
