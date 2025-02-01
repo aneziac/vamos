84163103e9fabfd3eb2828a36eed5bec461c3416
Nate Annau <nateannau@gmail.com>
2025-02-01 00:58:54 -0800

    Starting integrating transcription code

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 import whisper  # type: ignore
 from threading import Thread
+import transcribe
+from pydub import AudioSegment
 
 
 model = whisper.load_model('base')
@@ -16,8 +18,12 @@ def upload_file():
     if file.filename == '':
         return "No selected file", 400
 
+    file.save(f"./uploads/audio/{file.filename}")
+
+    audio = AudioSegment.from_file(file.stream, format='mp3')
+    transcribe.transcribe_with_whisper(audio, f"./uploads/transcripts/{file.filename}")
+
     # Save the file to a directory
-    file.save(f"./uploads/{file.filename}")
     return "File uploaded successfully", 200
 
 
--- backend/transcribe.py
+++ backend/transcribe.py
@@ -1,15 +1,15 @@
 import whisper
 
 def transcribe_with_whisper(input_file, output_dir):
-    # could be base/small/medium 
+    # could be base/small/medium
     model = whisper.load_model("base")
-    
+
     result = model.transcribe(
         input_file,
         language="en",
         word_timestamps=True,
     )
-    
+
     #transcribed_text = result["text"]
     segments = result["segments"]
 
@@ -19,19 +19,13 @@ def transcribe_with_whisper(input_file, output_dir):
             start_time = segment["start"]
             end_time = segment["end"]
             text = segment["text"]
-            
+
             # SRT time format (HH:MM:SS,MS)
             start_time_srt = f"{int(start_time // 3600):02}:{int((start_time % 3600) // 60):02}:{int(start_time % 60):02},{int((start_time * 1000) % 1000):03}"
             end_time_srt = f"{int(end_time // 3600):02}:{int((end_time % 3600) // 60):02}:{int(end_time % 60):02},{int((end_time * 1000) % 1000):03}"
-            
+
             f.write(f"{idx + 1}\n")
             f.write(f"{start_time_srt} --> {end_time_srt}\n")
             f.write(f"{text}\n\n")
-    
-    print(f"Transcription saved to: {srt_file}")
-    #print(transcribed_text)
-    #print(segments)
 
-input_audio_file = "audio.mp3"
-output_directory = "/Users/aq2003/vam"
-transcribe_with_whisper(input_audio_file, output_directory)
+    print(f"Transcription saved to: {srt_file}")
