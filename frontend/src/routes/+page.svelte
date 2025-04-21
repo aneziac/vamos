839fc503553e7760a3a89d39f5f68baa219508b6
aq2oo3 <hetarepianist@gmail.com>
2025-04-20 20:21:24 -0700

    AAAA 2

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,6 +2,112 @@
     // https://help.openai.com/en/articles/7031512-whisper-audio-api-faq
     const validExtensions = ['.m4a', '.mp3', '.webm', '.mp4', '.mpga', '.wav', '.mpeg'];
     const baseServerURL = 'http://127.0.0.1:5000'
+
+    import { audioData } from './audioData.js';
+    import TrackHeading from '$lib/TrackHeading.svelte';
+    import ProgressBarTime from '$lib/ProgressBarTime.svelte';
+    import Controls from '$lib/Controls.svelte';
+    import VolumeSlider from '$lib/VolumeSlider.svelte';
+    import { onMount } from 'svelte';
+
+    let trackIndex = 0;
+    let audioFile: HTMLAudioElement | null = null;
+    let trackTitle = audioData[trackIndex].name;
+    let totalTrackTime: number;
+
+    onMount(() => {
+        audioFile = new Audio(audioData[trackIndex].url);
+        audioFile.onloadedmetadata = () => {
+        totalTrackTime = audioFile!.duration;
+        updateTime();
+        };
+    });
+
+    let totalTimeDisplay = "loading...";
+	let currTimeDisplay = "0:00:00";
+	let progress = 0;
+	let trackTimer: NodeJS.Timeout;
+
+	function updateTime() {
+		if (audioFile) {
+			progress = audioFile.currentTime * (100 / totalTrackTime);
+			let currHrs = Math.floor((audioFile.currentTime / 60) / 60);
+			let currMins: any = Math.floor(audioFile.currentTime / 60);
+			let currSecs: any = Math.floor(audioFile.currentTime - currMins * 60);
+
+			let durHrs = Math.floor( (totalTrackTime / 60) / 60 );
+			let durMins: any = Math.floor( (totalTrackTime / 60) % 60 );
+			let durSecs: any =  Math.floor(totalTrackTime - (durHrs * 60 * 60) - (durMins * 60));
+
+			if (currSecs < 10) currSecs = `0${currSecs}`;
+			if (durSecs < 10) durSecs = `0${durSecs}`;
+			if (currMins < 10) currMins = `0${currMins}`;
+			if (durMins < 10) durMins = `0${durMins}`;
+
+			currTimeDisplay = `${currHrs}:${currMins}:${currSecs}`;
+			totalTimeDisplay = `${durHrs}:${durMins}:${durSecs}`;
+
+			if (audioFile.ended) {
+				toggleTimeRunning();
+			}
+		}
+
+		
+	}
+
+	const toggleTimeRunning = () => {
+		if (audioFile) {
+			if (audioFile.ended) {
+				isPlaying = false;
+				clearInterval(trackTimer);
+				console.log(`Ended = ${audioFile.ended}`);
+			} else {
+				trackTimer = setInterval(updateTime, 100);
+			}
+		}
+	}
+
+
+	// Controls
+	let isPlaying = false;
+	$: console.log(`isPlaying = ${isPlaying}`)
+
+	const playPauseAudio = () => {
+		if (audioFile) {
+			if (audioFile.paused) {
+				toggleTimeRunning()
+				audioFile.play();
+				isPlaying = true;
+			} else {
+				toggleTimeRunning()
+				audioFile.pause();
+				isPlaying = false;
+			}
+		}
+	}
+
+	const rewindAudio = () => {
+		if (audioFile) {
+			audioFile.currentTime -= 10;
+		}
+	};
+
+	const forwardAudio = () => {
+		if (audioFile) {
+			audioFile.currentTime += 10;
+		}
+	};
+
+	// const forwardAudio = () => audioFile.currentTime += 10;
+
+	// Volume Slider
+	let vol = 50;
+
+	const adjustVol = () => {
+		if (audioFile) {
+			audioFile.volume = vol / 100;
+		}
+	};
     
     let youtubeLink = '';
     let uploadedFile = false;
@@ -101,11 +207,24 @@
                 <button 
                     type="submit"
                     style="background-color: #6cb4ac;"
-                    class="text-white font-bold py-2 px-6 rounded-full shadow-lg transition-all duration-300 ease-in-out hover:scale-105"
+                    class="text-white font-bold py-2 px-6 rounded-full shadow-lg transition-all duration-300 ease-in-out hover:scale-105 mb-3"
                 >
                     Submit
                 </button>
             </div> 
+
+            <section id="player-cont">
+                <TrackHeading {trackTitle} />
+                <ProgressBarTime {currTimeDisplay}
+                                 {totalTimeDisplay}
+                                 {progress} />
+                <Controls {isPlaying}
+                    on:rewind={rewindAudio}
+                    on:playPause={playPauseAudio}
+                    on:forward={forwardAudio} />
+                <VolumeSlider bind:vol
+                              on:input={adjustVol} />
+            </section>
         </form>
     
         {#if statusMessage}
@@ -138,6 +257,16 @@
             right: auto;
             width: 75%;
             white-space: normal;
+        }
+
+        #player-cont {
+            width: 300px;
+            height: 165px;
+            padding: .7rem 1.5rem 0;
+            box-shadow: 0 0 5px black;
+            background: #222;
+            color: #bbb;
+            border-radius: 5px 5px 0 0;
         }
     
         .group{
---
+++ frontend/src/routes/audioData.js
@@ -0,0 +1,6 @@
+export const audioData = [
+	{
+		name: "The Practice of Giving",
+		url: "https://download.pariyatti.org/free/_moIbLs95/Dana_The_Practice_of_Giving_single.mp3",
+	}
+]
